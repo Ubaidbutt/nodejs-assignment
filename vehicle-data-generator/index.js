dc3d6dd0d8182d2ec5703b6e990588e6556bf755
Ubaid Butt <41114284+Ubaidbutt@users.noreply.github.com>
2022-12-27 19:59:34 +0100

    Add my comments for question1 in the data-generator file

--- vehicle-data-generator/index.js
+++ vehicle-data-generator/index.js
@@ -6,9 +6,9 @@ There are 2 question points for you to tell us the answer on your presentation
 If you're up for it
 
 */
-const csvParse      = require ( "csv-parse")
-const fs            = require ( "fs")
-const Writable      = require ("stream").Writable
+const csvParse = require("csv-parse")
+const fs = require("fs")
+const Writable = require("stream").Writable
 
 // NATS Server is a simple, high performance open source messaging system
 // for cloud native applications, IoT messaging, and microservices architectures.
@@ -21,7 +21,7 @@ const NATS = require("nats")
 
 // NATS connection happens here
 // After a connection is made you can start broadcasting messages (take a look at nats.publish())
-const nats = NATS.connect({json: true})
+const nats = NATS.connect({ json: true })
 
 // This function will start reading out csv data from file and publish it on nats
 const readOutLoud = (vehicleName) => {
@@ -32,6 +32,26 @@ const readOutLoud = (vehicleName) => {
 	// What's the difference betweeen fs.createReadStream, fs.readFileSync, and fs.readFileAsync?
 	// And when to use one or the others
 	// =========================
+	/*
+		readFileSync and readFile(Async) are both fully buffered process which means that the entire file content is read into the buffer
+		(temporary place in memory) & then processed. The only difference is that readFileSync is a blocking process which means the process
+		will be blocked until the entire file content is loaded in the memory whereas readFile(Async) is non-blocking process that takes a 
+		callback so that the process can perform other actions while the file content is being read & once it is read, it will execute the
+		callback function.
+
+		In a single threaded process, it is always preferred to use the Asynchornous version "readFile(Async)" so that the main thread is
+		not blocked for an I/O operation. Just imagine, your HTTP server blocking requests while it is busy reading a 500MB file.
+
+		However, there are cases where you can prefer using the synchornous version "readFileSync". For example, you can use readFileSync 
+		to load your SSL files before you start your server since you need to block until the files are ready.
+
+		createReadStream is a partially buffered process meaning that your entire file content is not loaded in the buffer, instead it gets
+		delivered as a stream of chunks. One chunk is loaded in the buffer, it gets processed, discarded from buffer & then the next chunk
+		is loaded in the buffer & so on and so forth.
+		The benefits of using streams are time and memory efficiency. NodeJS streams are powerful way of file read/write and network communication.
+		We can use async iterators with streams or nodejs provides piping mechanisms where we can pipe an output of one stream as an input
+		to another stream. (Like you have done in this code sample as well)
+	*/
 
 	// Now comes the interesting part,
 	// Handling this filestream requires us to create pipeline that will transform the raw string
@@ -56,7 +76,7 @@ const readOutLoud = (vehicleName) => {
 				setTimeout(() => {
 
 					i++
-					if((i % 100) === 0)
+					if ((i % 100) === 0)
 						console.log(`vehicle ${vehicleName} sent have sent ${i} messages`)
 
 					// The first parameter on this function is topics in which data will be broadcasted
