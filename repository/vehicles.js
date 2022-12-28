b82dc487c8fe10f14a690028f188eca0b16ac273
Ubaid Butt <41114284+Ubaidbutt@users.noreply.github.com>
2022-12-28 21:11:33 +0100

    update repository service to use the latest database utility function

--- repository/vehicles.js
+++ repository/vehicles.js
@@ -1,12 +1,15 @@
 const { connect } = require('../utilities/database');
+const config = require('./config/config');
+
+const { dbName, dbUrl } = config;
 
 async function addVehicle(data) {
-    const db = await connect();
+    const db = await connect({ dbUrl, dbName });
     return db.collection('vehicles').insertOne(data);
 }
 
 async function getVehicle(options, filters = {}) {
-    const db = await connect();
+    const db = await connect({ dbUrl, dbName });
     return db.collection('vehicles').find(filters).skip(options.offset).limit(options.limit).toArray();
 }
 
